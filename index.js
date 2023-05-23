693e22936f6fdd88a4f7417779881f7bc6da4c11
Gustavo Henrique da Silva <sykogus@gmail.com>
2023-05-23 16:34:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function checar() {
             switch (data.results.forecast[0].rain_probability) {
 
                 case 0:
-                    document.body.style.backgroundImage = "ulr(https://kaupalph.sirv.com/Imagens/imagens/03%20raios-chuva-clima-ao-vivo-tempo-alerta-previsao-1677503593653.jpgg)"
+                    document.body.style.backgroundImage = "url(https://kaupalph.sirv.com/Imagens/imagens/03%20raios-chuva-clima-ao-vivo-tempo-alerta-previsao-1677503593653.jpg)"
                         document.getElementById("texto").innerHTML = `Não, a probabilidade de chuva é ${chuva} %`
                     break;
 
