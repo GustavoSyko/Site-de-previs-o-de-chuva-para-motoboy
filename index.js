7074b6340d57ed762c029194f33b7baf04cad626
Gustavo Henrique da Silva <sykogus@gmail.com>
2023-05-23 19:57:52 -0300

    Alterado função switch para " if else"
    Percebi depois que só iria funcionar nos numero exatos, 0-25-50-75-100.
    Então foi necessário usar else if <= etc...

--- index.js
+++ index.js
@@ -4,49 +4,33 @@ function checar() {
         .then(response => response.json())
         .then(data => {
             const chuva = data.results.forecast[0].rain_probability;
-            switch (data.results.forecast[0].rain_probability) {
-
-                case 0:
-                    document.body.style.backgroundImage = "url()";
-                    document.getElementById("texto").innerHTML = `Não, a probabilidade de chuva é ${chuva} %`
-                    break;
-
-
-                case 25:
-                    document.body.style.backgroundImage = "url()"
-                    document.getElementById("texto").innerHTML = "Não"
-
-                    break;
-
-
-                case 50:
-                    document.body.style.backgroundImage = "url()"
-                    document.getElementById("texto").innerHTML = "Não"
-
-
-                    break;
-
-
-                case 75:
-                    document.body.style.backgroundImage = "url()"
-                    document.getElementById("texto").innerHTML = "Não"
-
-                    break;
-
-
-                case 100:
-                    document.body.style.backgroundImage = "url()"
-                    document.getElementById("texto").innerHTML = "Não"
-
-                    break;
-
-                default:
-
-                    break;
+            if (chuva <= 10) {
+                document.body.style.backgroundImage = "url(https://kaupalph.sirv.com/Imagens/imagens/verao-calor-vista-skyline-copan-1.webp)";
+                document.getElementById("texto").innerHTML = `Não, a probabilidade de chuva é de ${chuva} %.`
             }
-        })
-        .catch(error => {
-            // Trate qualquer erro que possa ocorrer
-            console.error('Ocorreu um erro ao tentar obter os dados da API:', error);
-        });
+
+            else if (chuva > 10 && chuva <= 30) {
+                document.body.style.backgroundImage = "url(https://kaupalph.sirv.com/Imagens/imagens/sol%20entre%20nuvens.jpg)"
+                document.getElementById("texto").innerHTML = `Poucas chances,a probabilidade é de ${chuva} % .`
+            }
+
+            else if (chuva > 30 && chuva <= 50) {
+                document.body.style.backgroundImage = "url()"
+                document.getElementById("texto").innerHTML = `Leva a capa! A probabilidade é de ${chuva} %.`
+            }
+
+            else if (chuva > 50 && chuva <= 80) {
+                document.body.style.backgroundImage = "url()"
+                document.getElementById("texto").innerHTML = "Não"
+            }
+
+            else if (chuva > 80 && chuva <= 100) {
+                document.body.style.backgroundImage = "url()"
+                document.getElementById("texto").innerHTML = "Não"
+            }})
+
+        .catch (error => {
+    // Trate qualquer erro que possa ocorrer
+    console.error('Ocorreu um erro ao tentar obter os dados da API:', error);
+});
 }
