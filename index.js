78b55b3e0cc55d4d7c771313d51cf0e1952a04f5
Gustavo Henrique da Silva <sykogus@gmail.com>
2023-05-23 13:18:05 -0300

    Trazer a resposta da API direto na função switch
    adicionado a função switch para pegar as diferentes porcentagens de probabilidade de chuva.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-import { Express } from "express";
+
+const url = `https://api.hgbrasil.com/weather?key=f3535208&user_ip=remote`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        switch (data.results.forecast[0].rain_probability) {
+
+            case  25:
+                console.log("Deu certo")
+                break;
+        }
+    })
+    .catch(error => {
+        // Trate qualquer erro que possa ocorrer
+        console.error('Ocorreu um erro ao tentar obter os dados da API:', error);
+    });
