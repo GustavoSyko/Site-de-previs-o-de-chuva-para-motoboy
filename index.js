778356a90e4d82d2d41d04f9d065ee1cde1d1ccf
Gustavo Henrique da Silva <sykogus@gmail.com>
2023-05-24 01:13:12 -0300

    Tentando fazer o fundo encaixar no mobile

--- index.js
+++ index.js
@@ -7,30 +7,35 @@ function checar() {
             if (chuva <= 10) {
                 document.getElementById("fontevideo").src ="https://kaupalph.sirv.com/cortados/sky_-_52170%20(1080p).mp4";
                 document.getElementById("videofundo").load();
-                document.getElementById("texto").innerHTML = `Não, a probabilidade de chuva é de ${chuva} %.`
+                document.getElementById("resultado").innerHTML = `Não, a probabilidade de chuva é de ${chuva} %.`
+                document.getElementById("texto").innerHTML = ""
             }
 
             else if (chuva > 10 && chuva <= 30) {
                 document.getElementById("fontevideo").src ="https://kaupalph.sirv.com/cortados/30%20a%2050.mp4";
                 document.getElementById("videofundo").load();
-                document.getElementById("texto").innerHTML = `Poucas chances,a probabilidade é de ${chuva} % .`
+                document.getElementById("resultado").innerHTML = `Poucas chances,a probabilidade é de ${chuva} % .`
+                document.getElementById("texto").innerHTML = ""
             }
 
             else if (chuva > 30 && chuva <= 60) {
                 document.getElementById("fontevideo").src ="https://kaupalph.sirv.com/cortados/132791%20(720p).mp4  ";
                 document.getElementById("videofundo").load();
-                document.getElementById("texto").innerHTML = `Leva a capa! A probabilidade é de ${chuva} %.`
+                document.getElementById("resultado").innerHTML = `Leva a capa! A probabilidade é de ${chuva} %.`
+                document.getElementById("texto").innerHTML = ""
             }
 
             else if (chuva > 60 && chuva <= 80) {
                 document.getElementById("fontevideo").src ="https://kaupalph.sirv.com/cortados/cidade%20noite%20chuva%20(2).mp4";
                 document.getElementById("videofundo").load();
-                document.getElementById("texto").innerHTML = `É meu amigo...acho que deu ruim. ${chuva} %.`
+                document.getElementById("resultado").innerHTML = `É meu amigo...acho que deu ruim. ${chuva} %.`
+                document.getElementById("texto").innerHTML = ""
             }
 
             else if (chuva > 80 && chuva <= 100) {
                 document.getElementById("fontevideo").src ="https://kaupalph.sirv.com/cortados/pexels-the-element-2657691-960x540-30fps.mp4";
                 document.getElementById("videofundo").load();
+                document.getElementById("resultado").innerHTML = ""
                 document.getElementById("texto").innerHTML = ""
             }
         })
