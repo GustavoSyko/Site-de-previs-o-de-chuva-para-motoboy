45524c0584d9fd28bf3d951e36603450e842c173
Gustavo Henrique da Silva <sykogus@gmail.com>
2023-05-24 00:29:49 -0300

    Adicionado videos
    Fiz upload dos videos para poder usar , cada video relacionado com o retorno.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function checar() {
         .then(data => {
             const chuva = data.results.forecast[0].rain_probability;
             if (chuva <= 10) {
-                document.getElementById("fontevideo").src ="https://kaupalph.sirv.com/cortados/pexels-cottonbro-studio-4605197-960x506-25fps.mp4";
+                document.getElementById("fontevideo").src ="https://kaupalph.sirv.com/cortados/sky_-_52170%20(1080p).mp4";
                 document.getElementById("videofundo").load();
                 document.getElementById("texto").innerHTML = `Não, a probabilidade de chuva é de ${chuva} %.`
             }
@@ -16,13 +16,13 @@ function checar() {
                 document.getElementById("texto").innerHTML = `Poucas chances,a probabilidade é de ${chuva} % .`
             }
 
-            else if (chuva > 30 && chuva <= 50) {
-                document.getElementById("fontevideo").src ="https://kaupalph.sirv.com/cortados/30%20a%2050.mp4";
+            else if (chuva > 30 && chuva <= 60) {
+                document.getElementById("fontevideo").src ="https://kaupalph.sirv.com/cortados/132791%20(720p).mp4  ";
                 document.getElementById("videofundo").load();
                 document.getElementById("texto").innerHTML = `Leva a capa! A probabilidade é de ${chuva} %.`
             }
 
-            else if (chuva > 50 && chuva <= 80) {
+            else if (chuva > 60 && chuva <= 80) {
                 document.getElementById("fontevideo").src ="https://kaupalph.sirv.com/cortados/cidade%20noite%20chuva%20(2).mp4";
                 document.getElementById("videofundo").load();
                 document.getElementById("texto").innerHTML = `É meu amigo...acho que deu ruim. ${chuva} %.`
